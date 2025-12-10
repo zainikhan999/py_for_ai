837181769bd87c691357ba5dd6b12270337413f0
zainikhan999 <zk.032918@gmail.com>
2025-12-10 18:53:06 +0500

    fixed logo size

--- src/app/components/home/NavBar.tsx
+++ src/app/components/home/NavBar.tsx
@@ -16,7 +16,7 @@ const Navbar: React.FC<NavbarProps> = ({ onGetStarted }) => {
           <img
             src="/py_ai_logo.png"
             alt="PyForAI Logo"
-            className="h-10 w-auto object-contain"
+            className="h-12 sm:h-14 md:h-16 w-auto object-contain"
           />
         </div>
 
